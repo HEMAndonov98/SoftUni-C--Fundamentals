c3a31b790ba10de69b78ecb93811d84fd3f36729
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-04-05 01:25:04 +0300

    Methods exercises

---
+++ Methods More Exercises/MethodsExercise/1.DataTypes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _1.DataTypes
+//    Write a program that, depending on the first line of the input, reads an int, double, or string.
+// If the data type is int, multiply the number by 2.
+// If the data type is real, multiply the number by 1.5 and format it to the second decimal point.
+// If the data type is a string, surround the input with &quot;$&quot;.
+//Print the result on the console.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+            string value = Console.ReadLine();
+
+            if (dataType == "int")
+            {
+                int number = int.Parse(value);
+                Console.WriteLine(ProcessValue(number));
+            }
+            else if (dataType == "real")
+            {
+                double realNum = double.Parse(value);
+                Console.WriteLine($"{ProcessValue(realNum):F2}");
+            }
+            else if (dataType == "string")
+            {
+                Console.WriteLine(ProcessValue(value));
+            }
+        }
+        static int ProcessValue(int number)
+        {
+            return number * 2;
+        }
+        static double ProcessValue(double realNum)
+        {
+            return realNum * 1.5;
+        }
+        static string ProcessValue(string word)
+        {
+            return $"${word}$";
+        }
+    }
+}
---
+++ Methods More Exercises/MethodsExercise/2.CenterPoint/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _2.CenterPoint
+{
+//    You are given the coordinates of two points on a Cartesian coordinate system - X1, Y1, X2, and Y2.Create a method
+//that prints the point that is closest to the center of the coordinate system (0, 0) in the format(X, Y). If the points are
+//at the same distance from the center, print only the first one.
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            ClosestPoint(x1, y1, x2, y2);
+        }
+
+        static void ClosestPoint(double x1, double y1, double x2, double y2)
+        {
+            double firstPoint = Math.Pow(x1, 2) + Math.Pow(y1, 2);
+            double secondPoint = Math.Pow(x2, 2) + Math.Pow(y2, 2);
+
+            if (firstPoint <= secondPoint)
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+        }
+    }
+}

983e3c80c1885be3bc8d9125f2f764ad7f22cd7c
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-04-25 15:43:53 +0300

    Refactored the code
    More readable,

--- MoreExercisesAssociativeArrays/3.MOBAChallenger/Program.cs
+++ MoreExercisesAssociativeArrays/3.MOBAChallenger/Program.cs
@@ -11,69 +11,81 @@ namespace _3.MOBAChallenger
             Dictionary<string, Dictionary<string, int>> players = new Dictionary<string, Dictionary<string, int>>();
 
             string input;
-            string[] tokens;
-
             while ((input = Console.ReadLine()) != "Season end")
             {
                 if (input.Contains("->"))
                 {
-                    tokens = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
-                    string player = tokens[0];
-                    string position = tokens[1];
-                    int skill = int.Parse(tokens[2]);
-
-                    if (!players.ContainsKey(player))
-                    {
-                        players[player] = new Dictionary<string, int>();
-                    }
-
-                    if (players[player].ContainsKey(position))
-                    {
-                        if (players[player][position] < skill)
-                        {
-                            players[player][position] = skill;
-                        }
-                    }
-
-                    if (!players[player].ContainsKey(position))
-                    {
-                        players[player][position] = skill;
-                    }
+                    AddPlayerAndPossition(players, input);
                 }
                 else if (input.Contains(" vs "))
                 {
-                    tokens = input.Split(" vs ", StringSplitOptions.RemoveEmptyEntries);
-                    string firstPlayer = tokens[0];
-                    string secondPlayer = tokens[1];
+                    Duel(players, input);
+                }
+            }
+            PrintOutput(players);
+        }
 
-                    if (!players.ContainsKey(firstPlayer) || !players.ContainsKey(secondPlayer))
-                    {
-                        continue;
-                    }
+        static void AddPlayerAndPossition(Dictionary<string, Dictionary<string , int>> players, string input)
+        {
+            string [] tokens = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+            string player = tokens[0];
+            string position = tokens[1];
+            int skill = int.Parse(tokens[2]);
 
-                    string playerToRemove = string.Empty;
-                    foreach (var firstPos in players[firstPlayer])
-                    {
-                        foreach (var secondPos in players[secondPlayer])
-                        {
-                            if (firstPos.Key == secondPos.Key)
-                            {
-                                if (players[firstPlayer].Values.Sum() > players[secondPlayer].Values.Sum())
-                                {
-                                    playerToRemove = secondPlayer;
-                                }
-                                else if (players[secondPlayer].Values.Sum() > players[firstPlayer].Values.Sum())
-                                {
-                                    playerToRemove = firstPlayer;
-                                }
-                            }
-                        }
-                    }
-                    players.Remove(playerToRemove);
-                    
+            if (!players.ContainsKey(player))
+            {
+                players[player] = new Dictionary<string, int>();
+            }
+
+            if (players[player].ContainsKey(position))
+            {
+                if (players[player][position] < skill)
+                {
+                    players[player][position] = skill;
                 }
             }
 
+            if (!players[player].ContainsKey(position))
+            {
+                players[player][position] = skill;
+            }
+        }
+
+        static void Duel(Dictionary<string, Dictionary<string, int>> players, string input)
+        {
+            string[] tokens = input.Split(" vs ", StringSplitOptions.RemoveEmptyEntries);
+            string firstPlayer = tokens[0];
+            string secondPlayer = tokens[1];
+
+            if (!players.ContainsKey(firstPlayer) || !players.ContainsKey(secondPlayer))
+            {
+                return;
+            }
+
+            string playerToRemove = string.Empty;
+            foreach (var firstPos in players[firstPlayer])
+            {
+                foreach (var secondPos in players[secondPlayer])
+                {
+                    if (firstPos.Key == secondPos.Key)
+                    {
+                        if (players[firstPlayer].Values.Sum() > players[secondPlayer].Values.Sum())
+                        {
+                            playerToRemove = secondPlayer;
+                        }
+                        else if (players[secondPlayer].Values.Sum() > players[firstPlayer].Values.Sum())
+                        {
+                            playerToRemove = firstPlayer;
+                        }
+                    }
+                }
+            }
+            players.Remove(playerToRemove);
+
+        }
+
+        static void PrintOutput(Dictionary<string, Dictionary<string, int>> players)
+        {
             players = players.OrderByDescending(x => x.Value.Values.Sum())
                 .ThenBy(x => x.Key)
                 .ToDictionary(x => x.Key, x => x.Value);

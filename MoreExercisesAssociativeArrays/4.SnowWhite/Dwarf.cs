daf525994f5b78e5de32da3c40fc83c90151c495
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-04-28 18:36:51 +0300

    Dwarf object in separate file

---
+++ MoreExercisesAssociativeArrays/4.SnowWhite/Dwarf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace _4.SnowWhite
+{
+    public class Dwarf
+    {
+        public Dwarf(string name, string color, int physics)
+        {
+            this.Name = name;
+            this.HatColor = color;
+            this.Physics = physics;
+        }
+
+        public string Name { get; set; }
+        public string HatColor { get; set; }
+        public int Physics { get; set; }
+    }
+}
--- MoreExercisesAssociativeArrays/4.SnowWhite/Program.cs
+++ MoreExercisesAssociativeArrays/4.SnowWhite/Program.cs
@@ -4,20 +4,6 @@ using System.Linq;
 
 namespace _4.SnowWhite
 {
-    class Dwarf
-    {
-        public Dwarf(string name, string color, int physics)
-        {
-            this.Name = name;
-            this.HatColor = color;
-            this.Physics = physics;
-        }
-
-        public string Name { get; set; }
-        public string HatColor { get; set; }
-        public int Physics { get; set; }
-    }
-
     class Program
     {
         static void Main(string[] args)

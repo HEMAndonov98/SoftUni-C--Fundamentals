6f7c79d90d98b010f4711f943527c16c502b221e
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-05-09 10:27:03 +0300

    finished first exercise

---
+++ More Exercises Strings and Text Processing/1.ExtractPersonInformation/2.AsciiSumator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2.AsciiSumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ More Exercises Strings and Text Processing/1.ExtractPersonInformation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1.ExtractPersonInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string namePtrn = @"@(\w+)\|";
+            string agePtrn = @"#(\d+)\*";
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match name = Regex.Match(input, namePtrn);
+                Match age = Regex.Match(input, agePtrn);
+
+                Console.WriteLine($"{name.Groups[1]} is {age.Groups[1]} years old.");
+            }
+        }
+    }
+}
